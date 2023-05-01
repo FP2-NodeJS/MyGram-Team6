795b3e9ff4ddf86fc8d99c74e127e3e3c599e70e
nnvi <vinnie.felim@gmail.com>
2023-05-02 01:36:44 +0700

    rev route

--- routers/usersRoute.js
+++ routers/usersRoute.js
@@ -3,33 +3,11 @@ const app = express()
 const {User} = require("../models")
 
 app.post('/login', (req,res)=>{
-    try{
-        const {email,full_name, username, password,profile_image_url,age,phone_number}= req.body
-        let data = User.create({
-            full_name,email, username, password,profile_image_url,age,phone_number
-        })
-        res.status(200).json({
-            user: data
-        })      
-    }catch(err){
-        res.status(400).json(err)
-    }
+    console.log('login user');
 })
 
 app.post('/register', (req,res)=>{
-    try{
-        const {email,full_name, username, password,profile_image_url,age,phone_number}= req.body
-        User.create({
-            full_name,email, username, password,profile_image_url,age,phone_number
-        }).then((result) => {
-            res.status(200).json(result)    
-        }).catch((err) => {
-            
-        });
-                 
-    }catch(err){
-        res.status(400).json(err)
-    }
+    console.log('register user');
 })
 
 app.put('/:userId', (req,res)=>{
